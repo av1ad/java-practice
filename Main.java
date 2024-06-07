3b7e1f8ef56aea2d1217bffa0f5ceaa26a8b614b
Aviad Churaman <aviadchuraman@gmail.com>
2024-06-06 20:24:21 -0400

    Test question

--- Main.java
+++ Main.java
@@ -1,3 +1,22 @@
 public class Main {
+    public static String reverseString(String str) {
+        if (str == null || str.isEmpty()) {
+            return str;
+        }
+
+        StringBuilder reversed = new StringBuilder();
+        for (int i = str.length() - 1; i >= 0; i--) {
+            reversed.append(str.charAt(i));
+        }
+
+        return reversed.toString();
+    }
+
+    public static void main(String[] args) {
+        String original = "Hello, World!";
+        String reversed = reverseString(original);
+        System.out.println("Original: " + original);
+        System.out.println("Reversed: " + reversed);
+    }
 
 }
